f5834e39df987e4096ee8374fbcb419f8abaf605
Alexander Petrov <acepetrov96@gmail.com>
2020-09-07 16:01:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,10 @@ function nowServing(arr) {
   }
   return `Currently serving ${arr.shift()}.`;
 }
+function currentLine(arr) {
+  if(arr.length === 0) {
+    return "The line is currently empty.";
+  } else if(arr.length === 1){
+    return `The line is currently: ${i}. ${arr[i]}`
+  }
+}
