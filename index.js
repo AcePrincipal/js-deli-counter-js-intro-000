2f2463107a54c996fbd995827ea89008a8d40f97
Alexander Petrov <acepetrov96@gmail.com>
2020-09-07 15:48:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ function takeANumber(arr, name) {
   return `Welcome, ${name}. You are number ${arr.length} in line.`;
 }
 function nowServing(arr) {
+  if(arr.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
   return arr.shift();
 }
